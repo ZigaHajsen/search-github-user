41f1f6420d1c7f86cd3cc9b33255d1f16347080b
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-12-05 19:36:19 +0100

    Added Login Page & style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import loginImg from '../images/login-img.svg';
+import styled from 'styled-components/macro';
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  return (
+    <Wrapper>
+      <div className='container'>
+        <img src={loginImg} alt='github user' />
+        <h1>github user</h1>
+        <button className='btn'>login</button>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+
+  img {
+    margin-bottom: 2rem;
+  }
+
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
 
 export default Login;
