385eeb9cd38b687b9b158c70bde369521b7bc443
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-12-05 19:24:32 +0100

    React Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Dashboard, Login, Error } from './pages';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact={true}>
+          <Dashboard />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
