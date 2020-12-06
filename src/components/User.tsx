64130e644d1a3bd7811e9cfe76d8e878f475aae5
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-12-06 11:26:12 +0100

    Added User component & styles

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Card, Followers } from '../components';
 
 const User: React.FC = () => {
-  return <div>User</div>;
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Card />
+        <Followers />
+      </Wrapper>
+    </section>
+  );
 };
+
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 export default User;
