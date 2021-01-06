1ff82f6665c521deb8cf6c6f08d1be2c53323ba0
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-06 19:11:59 +0100

    auth0 setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 import store from './redux';
 import App from './App';
 import { GlobalStyles } from './GlobalStyles';
@@ -8,9 +9,15 @@ import { GlobalStyles } from './GlobalStyles';
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN!}
+      clientId={process.env.REACT_APP_CLIENT_ID!}
+      redirectUri={window.location.origin}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
