20a799aa8122729e2a22d85ce1eee0f030e21818
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-08 00:22:59 +0100

    added auth wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Dashboard, Login, PrivateRoute, Error } from './pages';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        {/*
+    <AuthWrapper>
+      <Router>
+        <Switch>
+          {/*
  // @ts-ignore */}
-        <PrivateRoute path='/' exact={true}>
-          <Dashboard />
-        </PrivateRoute>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
+          <PrivateRoute path='/' exact={true}>
+            <Dashboard />
+          </PrivateRoute>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthWrapper>
   );
 };
 
--- src/pages/AuthWrapper.tsx
+++ src/pages/AuthWrapper.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import loadingGif from '../images/preloader.gif';
+import styled from 'styled-components/macro';
 
-const AuthWrapper: React.FC = () => {
-  return <div>Auth Wrapper</div>;
+const AuthWrapper: React.FC = ({ children }) => {
+  const { isLoading, error } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <img src={loadingGif} alt='spinner' />
+      </Wrapper>
+    );
+  }
+  if (error) {
+    return (
+      <Wrapper>
+        <h1>{error.message}</h1>
+      </Wrapper>
+    );
+  }
+  return <Fragment>{children}</Fragment>;
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+
+  img {
+    width: 150px;
+  }
+`;
 
 export default AuthWrapper;
