e174630c48beb4643b77f22ca92953b1a904407a
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-10 18:40:12 +0100

    updated GithubUserModel

--- src/models/interface-models.ts
+++ src/models/interface-models.ts
@@ -1,36 +1,16 @@
 export interface GithubUserModel {
-  login: string;
-  id: number;
-  node_id: string;
   avatar_url: string;
-  gravatar_id: string;
-  url: string;
   html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: false;
   name: string;
   company: null | string;
   blog: null | string;
   location: null | string;
-  email: null | string;
-  hireable: null | boolean;
   bio: null | string;
   twitter_username: null | string;
-  public_repos: number;
-  public_gists: number;
-  followers: number;
-  following: number;
-  created_at: string;
-  updated_at: string;
+  public_repos: null | number;
+  public_gists: null | number;
+  followers: null | number;
+  following: null | number;
 }
 
 export interface GithubFollowerModel {
