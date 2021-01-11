794cbe847d3e27c70c545eb6f47d53e6228d3989
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-11 11:58:04 +0100

    typed errors actions & reducer

--- src/redux/actions/errorsActions.ts
+++ src/redux/actions/errorsActions.ts
@@ -1,13 +1,28 @@
 import { actionTypes } from '../types';
+interface SetErrorAction {
+  type: typeof actionTypes.SET_ERROR;
+  payload: object;
+}
 
-export const setError = (show: boolean, msg: string) => (dispatch: any) => {
+interface RemoveErrorAction {
+  type: typeof actionTypes.REMOVE_ERROR;
+  payload: object;
+}
+
+export type ErrorsActions = SetErrorAction | RemoveErrorAction;
+
+export const setError = (show: boolean, msg: string) => (
+  dispatch: (action: ErrorsActions) => null
+) => {
   dispatch({
     type: actionTypes.SET_ERROR,
     payload: { show, msg },
   });
 };
 
-export const removeError = () => (dispatch: any) => {
+export const removeError = () => (
+  dispatch: (action: ErrorsActions) => null
+) => {
   dispatch({
     type: actionTypes.REMOVE_ERROR,
     payload: { show: false, msg: '' },
--- src/redux/reducers/errorsReducer.ts
+++ src/redux/reducers/errorsReducer.ts
@@ -1,11 +1,15 @@
 import { actionTypes } from '../types';
+import { ErrorsActions } from '../actions/errorsActions';
 
 const initialState = {
   show: false,
   msg: '',
 };
 
-export default function errorsReducer(state = initialState, action: any) {
+export default function errorsReducer(
+  state = initialState,
+  action: ErrorsActions
+) {
   switch (action.type) {
     case actionTypes.SET_ERROR:
       return action.payload;
